0625c9226de49c8e5af2762327bbfd719ddceb6c
Ronald <ronald.silva@smarti9.com.br>
2020-08-06 19:16:55 -0300

    Add Alert

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,17 +1,36 @@
 import React, { useState } from 'react';
-import {  StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import {  StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import AwesomeAlert from 'react-native-awesome-alerts';
+
 
 export default function Formulario(){
+    const [ paciente, guardaPaciente ] = useState('');
+    const [ dono, guardaDono ] = useState('');
+    const [ telefone, guardaTelefone ] = useState('');
+    const [ data, guardaData ] = useState('');
+    const [ hora, guardahora ] = useState('');
+    const [ sintomas, guardaSintomas ] = useState('');
 
-    const [data, guardaData] = useState('');
-    const [hora, guardahora] = useState('');
+    const [mostraAlert, setMostraAlert] = useState(false);
 
 
 
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
 
+
+    //Mostra ou oculta Alert
+
+    function showAlert(){
+        setMostraAlert(true)
+    }
+
+    function hideAlert(){
+        setMostraAlert(false)
+    }
+
+     //Mostra ou oculta Data Picke
     const showDatePicker = () => {
         setDatePickerVisibility(true);
     };
@@ -23,11 +42,10 @@ export default function Formulario(){
     const confirmaData = (date) => {
         const opcao = { year: 'numeric', month: 'long', day: '2-digit' }
         guardaData(date.toLocaleDateString('pt-BR', opcao))
-        console.warn("A date has been picked: ", date);
         hideDatePicker();
     };
 
-    //Mostra ou oculta Datatime Picker
+    //Mostra ou oculta Time Picker
     const showTimePicker = () => {
         setTimePickerVisibility(true);
     };
@@ -37,11 +55,39 @@ export default function Formulario(){
     };
 
     const confirmaHora = (hora) => {
-        console.log(hora);
         const opcao = { hour: 'numeric',minute: '2-digit',hour12: false }
         guardahora(hora.toLocaleTimeString('pt-BR', opcao))
         hideTimePicker();
     };
+
+    function criarNovo(){
+
+        if(paciente.trim() === ''
+          ||dono.trim() === '' 
+          ||sintomas.trim() === ''
+          ||telefone.trim() === ''
+          ||data.trim() === ''
+          ||hora.trim() === ''){  
+              
+            showAlert()
+            return;
+
+            // mostrarAlerta()
+            // return;
+          }
+
+    }
+
+    function mostrarAlerta(){
+        // Alert.alert(
+        //     'Erro', //Titulo
+        //     'Todos os campos são obrigátorios',//mensagem
+        //     [{
+        //         text: 'OK'
+        //     }]
+        // )      
+     
+    }
 
     return(
        <View style={styles.formulario}>
@@ -49,7 +95,7 @@ export default function Formulario(){
                 <Text style={styles.label}>Paciente:</Text>
                 <TextInput
                     style={styles.input}  
-                    onChangeText={(text) => console.log(text) }          
+                    onChangeText={(text) => guardaPaciente(text) }          
                 />
             </View>
 
@@ -57,7 +103,7 @@ export default function Formulario(){
                 <Text style={styles.label}>Dono:</Text>
                 <TextInput
                     style={styles.input}
-                    onChangeText={(text) => console.log(text) }            
+                    onChangeText={(text) => guardaDono(text) }            
                 />
             </View>
 
@@ -66,13 +112,13 @@ export default function Formulario(){
                 <TextInput
                     style={styles.input}
                     keyboardType='numeric' 
-                    onChangeText={(text) => console.log(text) }    
+                    onChangeText={(text) => guardaTelefone(text) }    
                 />
             </View>
             <View style={styles.view}>
                 <Text style={styles.labelDate}>{data}</Text>
                     <Button title="Selecionar Data" onPress={showDatePicker} />
-                    
+
                     <DateTimePickerModal
                         isVisible={isDatePickerVisible}
                         mode="date"
@@ -93,8 +139,7 @@ export default function Formulario(){
                         onCancel={hideTimePicker}
                         locale='pt_BR'
                         is24Hour
-                    />
-                
+                    />                
             </View>
 
             <View>
@@ -102,40 +147,69 @@ export default function Formulario(){
                 <TextInput
                     multiline
                     style={styles.input}   
-                    onChangeText={(text) => console.log(text) }         
+                    onChangeText={(text) => guardaSintomas(text) }         
                 />
             </View>
+
+            <View style={styles.btnSubmit}>
+                 <Button  onPress={() => criarNovo()} title="Salvar" />
+            </View>
+
+            {
+                mostraAlert && (
+                    <AwesomeAlert
+                    show={showAlert}
+                    title="Erro"
+                    message="Todos os campos são obrigatorios"          
+                    showCancelButton={false}
+                    showConfirmButton={true}                   
+                    confirmText="OK"
+                    confirmButtonColor="#2196F3"
+                    onCancelPressed={() => {
+                      hideAlert();
+                    }}
+                    onConfirmPressed={() => {
+                      hideAlert();
+                    }}
+                  />
+                )
+            }
+
+        
+
        </View>
     )
 }
-
 const styles = StyleSheet.create({
     formulario:{
         backgroundColor: '#FFF',
-        paddingHorizontal:20,
+        paddingHorizontal:10,
         paddingVertical: 10,
         marginHorizontal: '2.5%'
     },  
     label:{
         fontWeight: 'bold',
-        fontSize: 18,
-        marginTop:20
+        fontSize: 12,
+        marginTop:10
     },
     input:{
         marginTop:10,
-        height:50,
+        height:40,
         borderColor:'black',
         borderWidth:1,
         borderStyle: 'solid'
     },
     view:{
-        marginTop: 5
+        marginTop: 1
     },
     labelDate:{
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize: 18,
+        fontSize: 12,
         marginTop:20,
         textTransform: 'uppercase'
+    },
+    btnSubmit:{
+        marginTop: 15
     }
 })
